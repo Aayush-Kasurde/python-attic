c10f55ca6fbe38db4ef75e609b007e743e8779fe
Aayush Kasurde <aayush.kasurde@gmail.com>
2015-01-27 21:11:06 +0530

    Added loginexample

--- flaskapp/loginexample/app/views.py
+++ flaskapp/loginexample/app/views.py
@@ -6,3 +6,7 @@ from app import app
 def index():
 	user = { 'nickname' : 'Aayush' }
 	return render_template('index.html', title = 'Home', user = user)
+
+@app.route('/login')
+def login_view():
+        return render_template('login.html')
