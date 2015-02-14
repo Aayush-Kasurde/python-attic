4254de747e9b98d16f9522273f856ae8bc65556a
Aayush Kasurde <aayush.kasurde@gmail.com>
2015-02-14 17:32:03 +0530

    Added template system

--- flaskapp/loginexample/app/views.py
+++ flaskapp/loginexample/app/views.py
@@ -7,6 +7,10 @@ def index():
 	user = { 'nickname' : 'Aayush' }
 	return render_template('index.html', title = 'Home', user = user)
 
+@app.route('/about/')
+def aboutus():
+	return render_template('aboutus.html', title='About Us')
+
 @app.route('/login')
 def login_view():
         return render_template('login.html')
